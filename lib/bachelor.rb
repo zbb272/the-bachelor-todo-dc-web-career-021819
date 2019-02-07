a85fa84e71270dee7252afab46ffb5e15d8eb982
Zachary <zbberman@yahoo.com>
2019-02-07 16:55:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -15,13 +15,11 @@ end
 def get_contestant_name(data, occupation)
   contestant_name = ""
   data.each do | season_key, season_data |
-    if season_key == season 
       season_data.each do | contestant |
         if contestant["status"] == "Winner"
           winner_name = contestant["name"].split(" ")[0]
         end 
       end 
-    end 
   end 
   contestant_name
 end
