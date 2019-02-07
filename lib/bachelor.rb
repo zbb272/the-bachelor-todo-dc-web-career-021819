266867784679afccc390b96fcbdd76fdf247b03b
Zachary <zbberman@yahoo.com>
2019-02-07 17:04:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def get_average_age_for_season(data, season)
   data.each do | season_key, season_data |
     if season_key == season 
       season_data.each do | contestant |
-        average_age += contestant["age"]
+        average_age += contestant["age"].to_i 
         number_of_contestants += 1 
       end 
     end 
