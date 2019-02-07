ced31d1bd5de3049bdb46a0045cee5417f4245d9
Zachary <zbberman@yahoo.com>
2019-02-07 16:48:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do | season_key, season_data |
+    if season_key == season 
+      
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
