350e6a1279af7d2160ce34a7ce18b4297da1a071
Zachary <zbberman@yahoo.com>
2019-02-07 16:54:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,17 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = ""
+  data.each do | season_key, season_data |
+    if season_key == season 
+      season_data.each do | contestant |
+        if contestant["status"] == "Winner"
+          winner_name = contestant["name"].split(" ")[0]
+        end 
+      end 
+    end 
+  end 
+  contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
