80e6a0bb2d49f57a0b8db929b6595babc7963d7c
Zachary <zbberman@yahoo.com>
2019-02-07 16:58:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do | season_key, season_data |
+    season_data.each do | contestant |
+      if contestant["occupation"] == occupation
+        hometown_count += 1
+      end 
+    end 
+  end 
+  hometown_count
 end
 
 def get_occupation(data, hometown)
